5c4bada31cc9176eda74a046080aced788b9c69e
dave jiang <jiangmoran@wolongdata.com>
2019-07-15 11:05:14 +0800

    add lesson2

--- leeson2/sub_station_spider.py
+++ leeson2/sub_station_spider.py
@@ -5,9 +5,10 @@
 # @Author  : 蒋默然
 # @File    : sub_station_spider.py
 # -------------------------------------------------
+import json
 import logging
 import re
-
+import time
 import requests
 from pyquery import PyQuery as pq
 
@@ -24,18 +25,36 @@ def get_index(url):
         stations = pq(i)('.station')
 
         # title = re.findall('(\w+)',title)[0] if re.findall('\d+',title) else title
-        print(title)
+        # print(title)
         line = []
         for sta in stations:
             name = pq(sta)('.station .link').text()
             line.append(name)
         line_info_dict[title] = line
-    print(line_info_dict)
+    return line_info_dict
+
+
+def get_poi(line_info_dict):
+    location = set()
+    for i in line_info_dict:
+        for m in line_info_dict[i]:
+            location.add(m)
+    result = {}
+    for i in location:
+        poi_url = 'http://api.map.baidu.com/geocoder?address={}&output=json&key=37492c0ee6f924cb5e934fa08c6b1676&city=%E5%8C%97%E4%BA%AC%E5%B8%82'.format(i)
+        r = requests.get(poi_url)
+        js_data = json.loads(r.text)
+        print(js_data)
+        poi = js_data['result']['location']
+        print(poi)
+        result[i] = poi
+        time.sleep(0.1)
 
 
 def main():
     url = 'http://bj.bendibao.com/ditie/linemap.shtml'
-    get_index(url)
+    line_info = get_index(url)
+    get_poi(line_info)
 
 
 if __name__ == '__main__':
